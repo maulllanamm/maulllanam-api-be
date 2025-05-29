efc56ac4d4a7e7dd8cd0b9e34dea1faa51aa65b9
maulllanamm <maulllanamuhammad@gmail.com>
2025-05-29 18:28:48 +0700

    feat : about user

--- Service/UserService.cs
+++ Service/UserService.cs
@@ -1,4 +1,5 @@
 using maulllanam_api_be.Database;
+using maulllanam_api_be.DTO;
 using maulllanam_api_be.Entity;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,5 +17,18 @@ public class UserService : BaseService<User>, IUserService
     {
         return await _dbSet
             .FirstOrDefaultAsync(x => x.Email == email && !x.IsDeleted);
+    }
+
+    public async Task<AboutDTO> GetAbout(Guid id)
+    {
+        var user = await _dbSet.FirstOrDefaultAsync(x => x.Id == id && !x.IsDeleted);
+        return new AboutDTO
+        {
+            Name = user.Name,
+            Email = user.Email,
+            Summary = user.Summary,
+            Title = user.Title,
+            Phone = user.Phone
+        };
     }
 }
