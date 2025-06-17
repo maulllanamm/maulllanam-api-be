9375d4ef2da156f01bb47f19a7b2fe87bd04b9f9
Maulana Muhammad <maulllanamuhammad@gmail.com>
2025-06-17 16:28:20 +0700

    feat : allow CORS

--- Program.cs
+++ Program.cs
@@ -31,6 +31,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Panggil seeder
@@ -62,6 +73,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
